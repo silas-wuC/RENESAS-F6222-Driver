60d9146e998dcebf3d0956a80147dfdbccec9711
silas_wu <silas_wu@tmytek.com>
2026-07-01 16:49:27 +0800

    refactor(channel): move f6222_set_channel_enable to f6222_channel.c
    Expose channel enable as public API in dedicated translation unit.
    
    - Add src/f6222_channel.c with f6222_set_channel_enable
    - Remove static copy from f6222_init.c
    - Restore public declaration in f6222.h
    
    ## Change summary (auto-generated)
     include/f6222.h     |  7 +++++++
     src/f6222_channel.c | 26 ++++++++++++++++++++++++++
     src/f6222_init.c    | 22 ----------------------
     3 files changed, 33 insertions(+), 22 deletions(-)
    
    ## Files
    M	include/f6222.h
    A	src/f6222_channel.c
    M	src/f6222_init.c
    
    ## Line counts (added / removed)
    7	0	include/f6222.h
    26	0	src/f6222_channel.c
    0	22	src/f6222_init.c

--- src/f6222_init.c
+++ src/f6222_init.c
@@ -90,28 +90,6 @@ static f6222_status_t f6222_set_init_pattern(f6222_dev_t* dev, uint8_t chip_addr
     return F6222_OK;
 }
 
-static f6222_status_t f6222_set_channel_enable(f6222_dev_t* dev, uint8_t rf_load, uint8_t chip_addr, uint8_t ch,
-                                               bool enable) {
-    if (dev == NULL || dev->spi_xfer == NULL) return F6222_ERR_INVALID_ARG;
-    if (rf_load > F6222_RF_LOAD_IMMEDIATE || chip_addr > F6222_CHIP_ADDR_MAX || !F6222_CH_IS_VALID(ch))
-        return F6222_ERR_INVALID_ARG;
-
-    f6222_status_t st;
-    uint16_t val = 0;
-    uint8_t reg = (uint8_t)(F6222_REG_CH1_SET + (F6222_CH_TO_IDX(ch) * F6222_CH_STRIDE));
-
-    st = f6222_local_reg_read(dev, chip_addr, reg, &val);
-    if (st != F6222_OK) return st;
-
-    if (enable) {
-        val &= (uint16_t)(~F6222_CHn_SET_CH_PWD);
-    } else {
-        val |= F6222_CHn_SET_CH_PWD;
-    }
-
-    return f6222_local_reg_write(dev, rf_load, chip_addr, reg, val);
-}
-
 f6222_status_t f6222_init(f6222_dev_t* dev, uint8_t chip_addr) {
     if (dev == NULL || dev->spi_xfer == NULL) return F6222_ERR_INVALID_ARG;
     if (chip_addr > F6222_CHIP_ADDR_MAX) return F6222_ERR_INVALID_ARG;
